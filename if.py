97893b4c6c63ac3334aa5707ac494dae4b7774b8
Liridon <liridonhaziri06@gmail.com>
2025-10-01 23:43:15 +0200

    Just fixed

--- if.py
+++ if.py
@@ -12,10 +12,13 @@ def if_weather_is_good():
 def have_fun():
   print("Enjoy your walk bro!")
 def have_lunch():
-  print("have lunch")
-  lunch = input("Is lunch? (good/bad) ")
-  if lunch == "good":
-    return True
+  eat1 = input("Do you want to have lunch? (yes/no) ")
+  if eat1 == "yes":
+    lunch = input("Is lunch? (good/bad) ")
+    if lunch == "good":
+        return True
+    else:
+        return False
   else:
     return False
 
@@ -23,8 +26,14 @@ def eat():
   print("Yummy!")
   
 def go_to_bed():
-  print("Go sleep..")
-  print("Zzzzzzz...")
+  wanna_sleep = input("Do you want to go to bed? (yes/no) ")
+  if wanna_sleep == "yes":
+    print("Go sleep..")
+    print("Zzzzzzz...")
+    return True
+  else:
+    print('Stay awake then "_" !')
+    return False
 
 if if_weather_is_good():
     go_for_a_walk()
