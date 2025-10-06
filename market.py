ca89889dd38e55120c74d2e4777b7851f50e35ba
Liridon <liridonhaziri06@gmail.com>
2025-10-06 10:36:53 +0200

    Super-Market

---
+++ market.py
@@ -0,0 +1,257 @@
+import time
+name = input("Enter your name: ")
+cash = float(input("Enter your amount of money you want to spend: "))
+print(cash)
+payment = 0
+
+
+banka_account = 10
+shporta = []
+nothing = 0
+def market():
+    print("=======SUPER-MARKET=======")
+    print("===MAIN-DEPARTMENTS===")
+
+def bakery_depart(ss, rr, gg):
+    global cash
+    print("=======BAKERY-DEPARTMENT=======")
+    global shporta
+    global payment
+    while True:
+        products = {"white bread" : 0.50, "wholemeal" : 1}
+        which = input("Anything to buy?(no - exit) ")                
+        if which == "no":
+            print("Exited")
+            print(f"Your {cash}$ left.")
+            print(f"Your products so far: {shporta}")
+            break
+        elif which in products:
+            shporta.append(which)
+            cash -= products[which]
+            payment += products[which]
+
+            print(f"Your {cash}$ left.")
+            if cash <= 0:
+                print("No money left.")
+                break
+def meat_department(ss, sse, gg):
+    global cash
+    print("=======MEAT-DEPARTMENT=======")
+    global shporta
+    global payment
+    products = {"white meat" : 1.5, "red meat" : 2, "fish" : 3}
+    while True:
+        print(f"{products}$")
+        which = input("Which meat do you want(no - exit): ")
+        if which == "no":
+            print("Exited")
+            print(f"Your {cash}$ left.")
+            print(f"Your products so far: {shporta}")
+            break
+        elif which in products:
+            shporta.append(which)
+            cash -= products[which]
+            payment += products[which]
+
+            print(f"Your {cash}$ left.")
+            if cash <= 0:
+                print("No money left.")
+                break
+def jackets(eu, sh, gg):
+    global cash
+    global shporta
+    global payment
+    print("=======JACKETS-DEPARTMENT=======")
+
+    products = {
+        "leather jacket": 30,
+        "denim jacket": 25,
+        "hoodie": 15,
+        "woolen jacket" : 20
+    }
+    while True:
+        print(products)
+        which = input("What do you want to buy: ")
+        if which == "no":
+            print("Exited from Jackets Department.")
+            print(f"""
+Your moneys left: {cash}
+Your products so far: {shporta} """)
+            break
+        elif which in products:
+            shporta.append(which)
+            cash -= products[which]
+            payment += products[which]
+
+            print(f"""
+Your moneys left: {cash}
+Your products so far: {shporta} """)
+        elif cash <= 0:
+            print("No money left.")
+            break
+
+def jeans(ff, vv, gg):
+    global cash
+    global shporta
+    print("=======JEANS-DEPARTMENT=======")
+
+    global payment
+    products = {
+        "slim fit": 5,
+        "regular fit": 4,
+        "skinny": 6,
+        "straight cut": 5,
+        "ripped jeans": 7}
+    while True:
+        print(products)
+        which = input("What type of Jeans you want to buy: ")
+        if which == "no":
+            print("Exited from Jeans Department.")
+            print(f"""
+            Your moneys left: {cash}
+            Your products so far: {shporta} """)
+            break
+        elif which in products:
+            shporta.append(which)
+            cash -= products[which]
+            payment += products[which]
+
+            print(f"""
+Your moneys left: {cash}
+Your products so far: {shporta} """)
+        elif cash <= 0:
+            print("No money left.")
+            break
+def tshirts(ss, sh, gg):
+    global cash
+    global shporta
+    print("=======T-SHIRTS-DEPARTMENT=======")
+
+    global payment
+    products = {
+        "slim fit": 5,
+        "regular fit": 4,
+        "skinny": 6,
+        "straight cut": 5,
+        "oversized shirt" : 7}
+    while True:
+        print(products)
+        which = input("What type of T-shirts you want to buy: ")
+        if which == "no":
+            print("Exited from T-shirts Department.")
+            print(f"""
+Your moneys left: {cash}
+Your products so far: {shporta} """)
+            break
+        elif which in products:
+            shporta.append(which)
+            cash -= products[which]
+            payment += products[which]
+            print(f"""
+            Your moneys left: {cash}
+            Your products so far: {shporta} """)
+        elif cash <= 0:
+            print("No money left.")
+            break
+def clothing_department(nn, ss, gg):
+    deparments = ["Jackets", "Jeans", "T-shirts"]
+    print("=======CLOTHING-DEPARTMENT=======")
+
+    print(deparments)
+    while True:
+        which = input("In which department of clothing you want to continue: ")
+        if which == "no":
+            print("Exited from clothing department.")
+            break
+        elif which == "jackets":
+            jackets(cash, shporta, payment)
+        elif which == "jeans":
+            jeans(cash, shporta, payment)
+        elif which == "t-shirts":
+            tshirts(cash, shporta, payment)
+        elif cash <= 0:
+            print("No money left.")
+            break
+def tech(ss, sh, gg):
+    global cash
+    global shporta
+    print("=======TECH-DEPARTMENT=======")
+
+    global payment
+    products = {
+        "laptop": 250,
+        "computer": 700,
+        "television": 450,
+        "play station": 500,
+        }
+    while True:
+        print(products)
+        which = input("What do you want to buy: ")
+        if which == "no":
+            print("Exited from Tech Department.")
+            print(f"""
+Your moneys left: {cash}
+Your products so far: {shporta} """)
+            break
+        elif which in products:
+            shporta.append(which)
+            cash -= products[which]
+            payment += products[which]
+            print(f"""
+            Your moneys left: {cash}
+            Your products so far: {shporta} """)
+        elif cash <= 0:
+            print("No money left.")
+            break
+        elif which =="just watching":
+            print("You need to get out sir.")
+            break
+def arka(gg):
+    
+    global cash
+    if cash == 0:
+        cash += banka_account
+    print(f"""Exited
+The amount you have pay: {payment}$"
+Your products: {shporta}
+Thank you for believing in our Market.
+All the bests {name.capitalize()}!""")
+    time.sleep(1)
+    print(f"I still have {cash} left.")
+def blej(nn, gg):
+    global cash
+    global shporta
+    global name
+    while True:   
+        v = input("In which department you want to enter?(bakery/meat/clothing/tech or no to exit)\n ")  
+        if v == "bakery":
+            print("Let's go to bakery department?")
+            bakery_depart(cash, shporta, payment)
+            
+        elif v == "meat":
+            print("Let's go to meat department?")
+            meat_department(cash, shporta, payment)
+            
+        elif v == "clothing":
+           print("Let's go to clothing department?")
+           clothing_department(cash, shporta, payment)
+        elif v == "tech":
+           print("Let's go to tech department?")
+           tech(cash, shporta, payment)
+        elif v == "no":
+            print("Exited")
+            arka(payment)
+            break
+        else:
+            print("Not such department.")
+            break
+
+
+
+
+
+
+
+
+market()
+blej(cash, payment)
